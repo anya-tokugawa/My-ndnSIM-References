8841a5100b3842ff22b4269cc3902b131c3e1c49
Yuto Nakagawa <19676712+anya-tokugawa@users.noreply.github.com>
2022-06-11 22:10:32 +0900

    Update checkNameMethod.cpp

--- test-codes/checkNameMethod.cpp
+++ test-codes/checkNameMethod.cpp
@@ -1,19 +1,20 @@
-   std::cout 
+  Name name = interest.getName(); // or data.getName();
+  std::cout 
     << "TEST: name.getPrefix(n)"
-    << "\n-2:" << interest.getName().getPrefix(-2) 
-    << "\n-1:" << interest.getName().getPrefix(-1) 
-    << "\n0:" << interest.getName().getPrefix(0) 
-    << "\n1:" << interest.getName().getPrefix(1)
-    << "\n2:" << interest.getName().getPrefix(2)
+    << "\n-2:" << name.getPrefix(-2) 
+    << "\n-1:" << name.getPrefix(-1) 
+    << "\n0:" << name.getPrefix(0) 
+    << "\n1:" << name.getPrefix(1)
+    << "\n2:" << name.getPrefix(2)
     << "\nTEST: name.at(n)"
-    << "\n-2:" << interest.getName().at(-2) 
-    << "\n-1:" << interest.getName().at(-1) 
-    << "\n0:" << interest.getName().at(0) 
-    << "\n1:" << interest.getName().at(1)
-    << "\n2:" << interest.getName().at(2)
+    << "\n-2:" << name.at(-2) 
+    << "\n-1:" << name.at(-1) 
+    << "\n0:" << name.at(0) 
+    << "\n1:" << name.at(1)
+    << "\n2:" << name.at(2)
     << "\nTEST: name.getSubName(n)"
-    << "\n-2:" << interest.getName().getSubName(-2)
-    << "\n-1:" << interest.getName().getSubName(-1)
-    << "\n0:" << interest.getName().getSubName(0)
-    << "\n1:" << interest.getName().getSubName(1)
-    << "\n2:" << interest.getName().getSubName(2) << std::endl;
+    << "\n-2:" << name.getSubName(-2)
+    << "\n-1:" << name.getSubName(-1)
+    << "\n0:" << name.getSubName(0)
+    << "\n1:" << name.getSubName(1)
+    << "\n2:" << name.getSubName(2) << std::endl;
